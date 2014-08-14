a088c9ec4b354095eb788f4bbb3ad7f62e748bed
Matt Gauger <matt.gauger@gmail.com>
2014-08-14 10:19:26 -0500

    Remove old jobs

--- jobs/github_status.rb
+++
@@ -1,9 +0,0 @@
-require 'faraday'
-require 'json'
-
-github_api = 'https://status.github.com/api/last-message.json'
-
-SCHEDULER.every '1m', :first_in => 0 do
-  github_status = JSON.parse(Faraday.get(github_api).body)
-  send_event('github_status', github_status)
-end
